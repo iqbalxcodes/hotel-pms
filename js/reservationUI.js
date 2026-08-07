50aec1ff7c2a709f17d116532bd854bd2e43a606
Muhammad Iqbal Nurfalah <iqbalxcodes@gmail.com>
2026-08-07 17:01:35 +0000

    Add reservationDetail.js script and alert for new reservation function; refactor reservation controls CSS for improved layout

--- js/reservationUI.js
+++ js/reservationUI.js
@@ -523,6 +523,8 @@ function showDevMessage(feature){
 
 function createNewReservation(){
 
+    alert("NEW FUNCTION LOADED");
+
     window.location.href =
         "reservation-detail.html?new=true";
 
